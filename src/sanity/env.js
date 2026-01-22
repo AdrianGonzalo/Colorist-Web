7a608b86d7215133e9c8e837e4d6c98179b3756e
Adrian <adriangonzalodev@gmail.com>
2026-01-22 12:40:51 +0100

    fix error

--- src/sanity/env.js
+++ src/sanity/env.js
@@ -1,3 +1,12 @@
 export const apiVersion = process.env.SANITY_STUDIO_API_VERSION || '2026-01-21'
 export const dataset = process.env.SANITY_STUDIO_DATASET
 export const projectId = process.env.SANITY_STUDIO_PROJECT_ID
+
+
+if (!projectId) {
+    throw new Error('Missing NEXT_PUBLIC_SANITY_PROJECT_ID')
+}
+
+if (!dataset) {
+    throw new Error('Missing NEXT_PUBLIC_SANITY_DATASET')
+}
