718bb7768111190a05508c920feb912ebe4773c5
Adrian <adriangonzalodev@gmail.com>
2026-01-22 14:11:08 +0100

    fix error

--- web/src/lib/sanity.js
+++ web/src/lib/sanity.js
@@ -1,8 +1,15 @@
 import { createClient } from "@sanity/client";
 
+const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID;
+const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET;
+
+if (!projectId || !dataset) {
+    throw new Error("Missing Sanity environment variables");
+}
+
 export const sanityClient = createClient({
-    projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-    dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
+    projectId,
+    dataset,
     apiVersion: "2024-01-01",
     useCdn: true,
 });
