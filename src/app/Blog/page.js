188d63f109e23157052cbb2508f2d04f1fdbd8fa
Adrian <adriangonzalodev@gmail.com>
2026-01-22 13:13:40 +0100

    todo limpio, vuelta a empezar para separar sanity de next

--- src/app/Blog/page.js
+++ src/app/Blog/page.js
@@ -1,7 +1,3 @@
-// src/app/Blog/page.jsx
-import { sanityClient } from "@/sanity/client.server";
-import PostList from "./PostList"; // importamos el componente client
-
 export default async function Blog() {
     const query = `*[_type == "post"] | order(publishedAt desc){
     title,
@@ -11,17 +7,11 @@ export default async function Blog() {
     content
   }`;
 
-    const posts = await sanityClient.fetch(query);
+
 
     return (
         <div className="mt-20 text-white px-4 md:px-20">
-            <h1 className="text-4xl font-bold mb-10 text-center">Blog</h1>
-
-            {posts.length === 0 ? (
-                <p className="text-center text-gray-400">No hay posts todavía.</p>
-            ) : (
-                <PostList posts={posts} />
-            )}
+            dfghdfgdfgdg
         </div>
     );
 }
--- src/sanity/client.js
+++
@@ -1,8 +0,0 @@
-// import { createClient } from '@sanity/client'
-
-// export const sanityClient = createClient({
-//     projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID, // desde tu .env.local de Next.js
-//     dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
-//     apiVersion: '2026-01-21', // usa la fecha actual
-//     useCdn: true, // `true` si quieres que sea rápido, no necesario para contenido nuevo
-// })
